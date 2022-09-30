75107968277441a92f0679056bb970ae80d70e80
Jerry Fidel <jerryfidel6@example.com>
2022-09-30 01:09:04 -0700

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    copy = a_dictionary.copy()
+    for i in copy:
+        copy[i] = copy.get(i) * 2
 
-def best_score(a_dict):
-    """
-    a function that returns a key with the biggest integer value
-    """
-    if a_dict is None or a_dict == {}:
-        return None
-    for key in a_dict.keys():
-        if a_dict[key] == max(a_dict.values()):
-            return 
+    return copy
